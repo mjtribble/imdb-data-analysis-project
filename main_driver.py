c42a895d99fc5e07cacdb0e1e572aac0370ba00d
Melody <mjtribble2@gmail.com>
2017-11-26 15:52:06 -0700

    question 4 query working for countries and genres

--- main_driver.py
+++ main_driver.py
@@ -17,7 +17,8 @@ def query():
 
     cursor = config.cursor()
 
-    # # Do the number of parts an actor works on increase, decrease or stay the same with age?
+    # EXECUTE AND PRINT QUERY 1
+    # Do the number of parts an actor works on increase, decrease or stay the same with age?
     # query1 = ("SELECT * "
     #           "FROM EMPLOYEE "
     #           "WHERE employee.Super_ssn IS NOT NULL;")
@@ -25,54 +26,73 @@ def query():
     # EXECUTE AND PRINT QUERY 2
     # # As a movie's budget increases do the sales also continuously increase
     # # query a movie budget and sales.
-    query2 = ("SELECT Primary_title, Total_gross , Budget "
-              "FROM MOVIE, TITLE "
-              "WHERE TM_const=T_const AND NOT Total_gross=0 AND NOT Budget=0 "
-              "ORDER BY Budget "
-              )
-
+    # query2 = ("SELECT Primary_title, Total_gross , Budget "
+    #           "FROM MOVIE, TITLE "
+    #           "WHERE TM_const=T_const AND NOT Total_gross=0 AND NOT Budget=0 "
+    #           "ORDER BY Budget "
+    #           )
+    #
     # cursor.execute(query2)
-    query2_dict = {}
-
+    # query2_dict = {}
+    #
     # for (Primary_title, Total_gross, Budget) in cursor:
     #     print("{}, {}, {}".format(Primary_title, Total_gross, Budget))
     #     query2_dict.update({Primary_title: (Total_gross, Budget)})
 
     # EXECUTE AND PRINT QUERY 3
     # Can we predict a genre based on the actor and director of a film
-    query3 = ('SELECT Genre, ACTOR_N_const, DIRECTOR_N_const '
-              'FROM ACTOR_HAS_ROLE_IN_TITLE AS a, TITLE_GENRE, DIRECTOR_DIRECTS_A_TITLE AS d '
-              'WHERE a.TITLE_T_const = TG_const and a.TITLE_T_const = d.TITLE_T_const and d.TITLE_T_const= TG_const ')
-
-    cursor.execute(query3)
+    # query3 = ('SELECT Genre, ACTOR_N_const, DIRECTOR_N_const '
+    #           'FROM ACTOR_HAS_ROLE_IN_TITLE AS a, TITLE_GENRE, DIRECTOR_DIRECTS_A_TITLE AS d '
+    #           'WHERE a.TITLE_T_const = TG_const and a.TITLE_T_const = d.TITLE_T_const and d.TITLE_T_const= TG_const ')
+    #
+    # cursor.execute(query3)
 
     # This dictionary has genre as a key and a list of actors and directors associated with that genre
-    query3_dict = {}
-
-    for (Genre, ACTOR_N_const, DIRECTOR_N_const) in cursor:
-        # print("{}, {}, {}".format(Genre, ACTOR_N_const, DIRECTOR_N_const))
-        if Genre in query3_dict:
-            query3_dict[Genre].append(ACTOR_N_const)
-            query3_dict[Genre].append(DIRECTOR_N_const)
-        else:
-            query3_dict.update({Genre: [ACTOR_N_const, DIRECTOR_N_const]})
-
-    print(query3_dict)
-
-    # # What is the probability that a particular genre is more popular in the U.S. vs. other countries?
-    # query4 = ("SELECT * "
-    #           "FROM EMPLOYEE "
-    #           "WHERE employee.Super_ssn IS NOT NULL;")
+    # query3_dict = {}
     #
+    # for (Genre, ACTOR_N_const, DIRECTOR_N_const) in cursor:
+    #     # print("{}, {}, {}".format(Genre, ACTOR_N_const, DIRECTOR_N_const))
+    #     if Genre in query3_dict:
+    #         query3_dict[Genre].append(ACTOR_N_const)
+    #         query3_dict[Genre].append(DIRECTOR_N_const)
+    #     else:
+    #         query3_dict.update({Genre: [ACTOR_N_const, DIRECTOR_N_const]})
+    #
+    # print(query3_dict)
+
+    # EXECUTE AND PRINT QUERY 4
+    # What is the probability that a particular genre is more popular in the U.S. vs. other countries?
+    query4 = ("SELECT Country, Genre "
+              "FROM TITLE_GENRE, MOVIE_COUNTRIES "
+              "WHERE TC_const = TG_const ")
+
+    cursor.execute(query4)
+
+    # not sure how we want this data, right now it is creating a list of genres for a particular country.
+    query4_dict = {}
+
+    for (Country, Genre ) in cursor:
+        # print("{}, {}, {}".format(Genre, ACTOR_N_const, DIRECTOR_N_const))
+        if Country in query4_dict:
+            query4_dict[Country].append(Genre)
+        else:
+            query4_dict.update({Country: [Genre]})
+
+    print(query4_dict)
+    # for response in cursor:
+    #     print(response)
+
+
+    # EXECUTE AND PRINT QUERY 5
     # # Can we predict a director based on actors, genre, budget, gross, and country of a film?
     # query5 = ("SELECT * "
     #           "FROM EMPLOYEE "
     #           "WHERE employee.Super_ssn IS NOT NULL;")
 
-    for response in cursor:
-        print(response)
-
-    config.close()
+    # for response in cursor:
+    #     print(response)
+    #
+    # config.close()
 
 
 if __name__ == '__main__':
