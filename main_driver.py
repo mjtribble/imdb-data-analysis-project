bd8d9078b2eaa5d280a74cc1ec4097de57d34536
Melody <mjtribble2@gmail.com>
2017-11-26 13:00:04 -0700

    removed the zero resultes form budget and gross

--- main_driver.py
+++ main_driver.py
@@ -13,19 +13,20 @@ import pymysql
 
 
 def query():
-    # config = {
-    #     'user': 'root',
-    #     'password': '*light*Bright',
-    #     'host': 'localhost',
-    #     'database': 'IMBD',
-    # }
-    #
-    # conn = pymysql.connect(**config)
-    # cursor = conn.cursor()
-    #
-    # for response in cursor:
-    #     print(response)
-    #
+
+
+    config = pymysql.connect("localhost", "root", "*light*Bright", "IMBD")
+    #    'user': 'root',
+    #    'password': 'Montana12',
+    #    'host': 'localhost',
+    #    'database': 'mydb',
+    #    'raise_on_warnings': True,
+    # )
+
+    # cnx = pymysql.connect(**config)
+
+    cursor = config.cursor()
+
     # # Do the number of parts an actor works on increase, decrease or stay the same with age?
     # query1 = ("SELECT * "
     #           "FROM EMPLOYEE "
@@ -33,10 +34,9 @@ def query():
     #
     # # As a movie's budget increases do the sales also continuously increase
     # # query a movie budget and sales.
-    #
-    # query2 = ("SELECT Total_gross, Budget"
-    #           "FROM MOVIE"
-    #           "WHERE movie.Total_gross IS NOT NULL;")
+    query2 = ("SELECT Total_gross , Budget "
+              "FROM MOVIE "
+              "WHERE NOT Total_gross=0 AND NOT Budget=0 ")
     #
     # # Can we predict a genre based on the actor and director of a film
     # query3 = ("SELECT * "
@@ -52,42 +52,14 @@ def query():
     # query5 = ("SELECT * "
     #           "FROM EMPLOYEE "
     #           "WHERE employee.Super_ssn IS NOT NULL;")
-    #
-    # cursor.execute(query2)
-    #
-    # for (Total_gross, Budget) in cursor:
-    #     print("{}, {}".format(Total_gross, Budget))
-    #
-    # cursor.close()
-    #
-    # conn.close()
-    #
-    #
-
-    config = pymysql.connect("localhost", "root", "*light*Bright", "IMBD")
-    #    'user': 'root',
-    #    'password': 'Montana12',
-    #    'host': 'localhost',
-    #    'database': 'mydb',
-    #    'raise_on_warnings': True,
-    # )
-
-    # cnx = pymysql.connect(**config)
-
-    cursor = config.cursor()
-
-    query2 = ("SELECT Total_gross , Budget "
-              "FROM MOVIE")
 
     cursor.execute(query2)
 
-    # for (TM_const, Language, Total_gross, Budget, Facebook_likes, IMDB_score) in cursor:
-    #     print("{}, {}, {}, {}, {}, {}".format(TM_const, Language, Total_gross, Budget, Facebook_likes, IMDB_score))
-    # for (Total_gross, Budget) in cursor:
-    #     print("{}, {}".format(Total_gross, Budget))
+    for (Total_gross, Budget) in cursor:
+        print("{}, {}".format(Total_gross, Budget))
 
-    for response in cursor:
-        print(response)
+    # for response in cursor:
+    #     print(response)
 
     config.close()
 
