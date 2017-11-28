66f5c51dffef1cbb25fbfbeb6a7418bbcaea6040
Melody <mjtribble2@gmail.com>
2017-11-27 17:18:54 -0700

    Question 2 finished with test, training, and residuals graphed

--- main_driver.py
+++ main_driver.py
@@ -3,12 +3,7 @@ Created on November 14, 2017
 @author: Melody Tribble & Xuying Wang
 
 """
-
-import correlation
-import classification
-import regression
 import pandas as pd
-from pandas import DataFrame
 import seaborn as sns
 import pymysql
 import regression
@@ -112,7 +107,3 @@ if __name__ == '__main__':
     # data = pd.read_csv("INSERT FILE PATH HERE")
     #
     # data.head()
-
-
-def visualize(d, x_vars, y_vars):
-    sns.pairplot(d, x_vars, y_vars, size=7, aspect=0.7, kind='reg')
--- regression.py
+++ regression.py
@@ -6,9 +6,12 @@ Created on November 14, 2017
 
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn import model_selection
 
 
-
+# This class runs linear regression model on the gross sales and budget of films from the imdb database using this
+# tutorial http://bigdata-madesimple.com/how-to-run-linear-regression-in-python-scikit-learn/
 class LRegression:
     def __init__(self, data):
         self.data = data
@@ -16,21 +19,52 @@ class LRegression:
         self.run_regression()
 
     def run_regression(self):
-
         # this removes the gross column from the data frame and sets it to X
-        X = self.data.drop('Gross', axis=1)
+        x = self.data.drop('Gross', axis=1)
 
         # this creates a linear regression object
         lm = LinearRegression()
-        lm.fit(X, self.data.Gross)
+        lm.fit(x, self.data.Gross)
 
-        print('Estimated intercept coefficient: ', lm.intercept_)
-        print('Number of coefficients: ', len(lm.coef_))
-
-        # pd.DataFrame(zip(X.columns, lm.coef_), columns=['features', 'estimatedCoefficients'])
-        plt.scatter(self.data.Budget, self.data.Gross)
+        # plot relationship b/w budget and gross
+        plt.scatter(self.data.Budget, self.data.Gross, s=10)
         plt.xlabel("Total film budget USD")
         plt.ylabel("Total film gross USD")
         plt.title("Relationship between Budget and Gross Sales")
         plt.show()
 
+        # plot predicted and actual gross sales
+        plt.scatter(self.data.Gross, lm.predict(x), s=10)
+        plt.xlabel("Gross sales: $Y_i$")
+        plt.ylabel("Predicted gross sales: $\hat{Y}_i$")
+        plt.title("Gross sales vs Predicted gross sales $Y_i$ vs $\hat{Y}_i$")
+        plt.show()
+
+        # calculate mean squared error
+        mse = np.mean((self.data.Gross - lm.predict(x)) ** 2)
+        print('Mean Squared error = ', mse)
+
+        # train data sets
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(
+            x, self.data.Gross, test_size=0.33, random_state=5)
+        print(x_train.shape)
+        print(x_test.shape)
+        print(y_train.shape)
+        print(y_test.shape)
+
+        # calculate mse for training and test data
+        lm.fit(x_train, y_train)
+        pred_train = lm.predict(x_train)
+        pred_test = lm.predict(x_test)
+        print("Fit a model x_train and calculate mse with y_train:",
+              np.mean((y_train - pred_train) ** 2))
+        print("Fit a model x_train and calculate mse with x_test, y_test:",
+              np.mean((y_test - pred_test) ** 2))
+
+        # visualize residual plot
+        plt.scatter(pred_train, pred_train - y_train, c='b', s=5, alpha=0.5)
+        plt.scatter(pred_test, pred_test - y_test, c='g', s=5)
+        plt.hlines(y=0, xmin=0, xmax=50)
+        plt.title("Residual Plot using training (blue) and test (green) data")
+        plt.ylabel("Residuals")
+        plt.show()
