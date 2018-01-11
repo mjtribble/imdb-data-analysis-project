89d6ec3561b16113c1a7d86b1d4017bcbd15de8f
Melody <mjtribble2@gmail.com>
2018-01-10 20:12:34 -0700

    regression change

--- regression.py
+++ regression.py
@@ -7,6 +7,7 @@ Created on November 14, 2017
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import numpy as np
+from scipy import stats
 from sklearn import model_selection
 
 
@@ -29,30 +30,31 @@ class LRegression:
     def run_regression(self):
         # budget =  x
         x = self.data.drop('Gross', axis=1)
+        x2 = np.array(x)
+        x1 = []
+        for i in x2:
+            for j in i:
+                x1.append(j)
+        x1 = np.array(x1)
+        y = np.array(self.data.Gross)
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x1, y)
+
+        print("slope = ", slope)
+        print("r_value = ", r_value)
+        print("p_value = ", p_value)
+        print("standard error = ", std_err)
+
+        # x = np.array(x)
+        # y = np.array(self.data.Gross)
 
         # this creates a linear regression object
         lm = LinearRegression()
         lm.fit(x, self.data.Gross)
 
-        # plot relationship b/w budget and gross
-        plt.scatter(self.data.Budget, self.data.Gross, s=10)
-        plt.xlabel("Total film budget (million USD)")
-        plt.ylabel("Total film gross (million USD)")
-        plt.title("Relationship between Budget and Gross Sales")
-        plt.show()
-
-        # plot predicted and actual gross sales
-        plt.scatter(self.data.Gross, lm.predict(x), s=10)
-        plt.xlabel("Gross sales: $Y_i$")
-        plt.ylabel("Predicted gross sales: $\hat{Y}_i$")
-        plt.title("Gross sales vs Predicted gross sales $Y_i$ vs $\hat{Y}_i$")
-
-        # visualize
-        plt.show()
-
-        # calculate mean squared error
-        mse = np.mean((self.data.Gross - lm.predict(x)) ** 2)
-        print('Mean Squared error = ', mse)
+        # # calculate mean squared error
+        # mse = np.mean((self.data.Gross - lm.predict(x)) ** 2)
+        # print('Mean Squared error = ', mse)
 
         # train data sets
         x_train, x_test, y_train, y_test = model_selection.train_test_split(
@@ -67,10 +69,69 @@ class LRegression:
         print("Fit a model x_train and calculate mse with x_test, y_test:",
               np.mean((y_test - pred_test) ** 2))
 
+        # plot relationship b/w budget and gross
+        plt.scatter(self.data.Budget, self.data.Gross, s=10)
+        plt.xlabel("Total film budget (100 million USD)")
+        plt.ylabel("Total film gross (100 million USD)")
+        plt.title("Gross Sales per Budget (Linear Regression)")
+        plt.plot(x_test, lm.predict(x_test), color='black', linewidth=3)
+        plt.show()
+
+        # plot predicted and actual gross sales
+        plt.scatter(self.data.Gross, lm.predict(x), s=10)
+        plt.xlabel("Actual Gross sales")
+        plt.ylabel("Predicted gross sales")
+        plt.title("Gross sales vs Predicted gross sales")
+        plt.plot(x_test, lm.predict(x_test), color='black', linewidth=3)
+
+        # visualize
+        plt.show()
+
         # visualize residual plot
         plt.scatter(pred_train, pred_train - y_train, c='b', s=5, alpha=0.5)
         plt.scatter(pred_test, pred_test - y_test, c='g', s=5)
         plt.hlines(y=0, xmin=0, xmax=50)
-        plt.title("Residual Plot using training (blue) and test (green) data")
+        plt.title("Linear Regression Residual Error Plot\n"
+                  "training (blue), test (green) data")
         plt.ylabel("Residuals")
         plt.show()
+
+#
+#
+# class Regression:
+# def __init__(self, list1, list2):
+# self.list1 = list1;
+# self.list2 = list2;
+# print ('Linear Regression:')
+# self.run()
+#
+#
+# def run(self):
+# #creates a pandas data frame which makes the data compatabel with sklearn
+# X = pd.DataFrame(self.list1)
+# Y = pd.DataFrame(self.list2)
+#
+# #this will print numerical values in the command line in respect to the scatter plot created below
+# lin = linregress(self.list1, self.list2)
+# print (str(lin))
+#
+# #creating test and train values for the model to test and train
+# X_train = X[:-250]
+# X_test = X[-250:]
+#
+# Y_train = Y[:-250]
+# Y_test = Y[-250:]
+#
+# #This will create a 2-d scatter plot of the test data
+# #It also is naming the cooridinates of the X and Y axis of the graph, overall title of the graph and the colors that the scatter plot will show
+# plt.scatter(X_test,Y_test,  color='blue')
+# plt.xlabel('Meta Data Score')
+# plt.ylabel('IMDB Rating')
+# plt.title('Relationship of MetaData and IMDB Ratings')
+#
+# #This method will fit the data and also show the line of regression on the scatter plot created above
+# lm = linear_model.LinearRegression()
+# lm.fit(X_train,Y_train)
+# plt.plot(X_test, lm.predict(X_test), color='red',linewidth=3)
+# plt.show()
+
